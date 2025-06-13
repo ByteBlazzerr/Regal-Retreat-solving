f9e37759d518f897c7cfc978395dbb615fc02390
Aakanksha Ghodki <aakankshaghodki@gmail.com>
2025-06-13 20:58:20 +0530

    console.log in middleware

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -6,19 +6,24 @@ export const protect =async(req,res,next)=>{
     console.log("authMiddleware called");
     try{
         const {userId}=req.auth();
+        console.log("User ID from auth:", userId); // Log the userId for debugging
         if(!userId){
             return res.json({
                 success:false,
                 message:"Not authenticated"
             });
         }
+        console.log("Fetching user with ID:", userId); // Log the userId before fetching
+         // Fetch the user from the database
         const user=await User.findById(userId);
+        console.log("Fetched user:", user); // Log the fetched user for debugging
         if(!user){
             return res.status(404).json({
                 success:false,
                 message:"User not found"
             })
         }
+        console.log("User found:", user); // Log the user object
         req.user=user;
         next();
     }
